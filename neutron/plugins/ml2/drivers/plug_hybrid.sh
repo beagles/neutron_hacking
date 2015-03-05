e78a54c77f1f7f5239e119daf0e88069eec76a11
Brent Eagles <beagles@redhat.com>
2015-03-05 10:40:34 -0330

    Add plug/unplug to test driver script
    Add unplug part as well as fix some typos.
    
    Change-Id: I65d1df84408c4012bff75794014d0ea96ff8354e

--- neutron/plugins/ml2/drivers/plug_hybrid.sh
+++ neutron/plugins/ml2/drivers/plug_hybrid.sh
@@ -13,40 +13,50 @@ export PATH=/usr/sbin:/sbin:/usr/bin:/bin
 
 BASE_ID="${VIF_ID:0:11}"
 
-BRIDGE_NAME="qvb-$BASE_ID"
+BRIDGE_NAME="qbr$BASE_ID"
 
 # Create bridge if it doesn't exist
-if ! [ ip link show $BRIDGE_NAME 2>/dev/null ];
+if ! [sudo ip link show $BRIDGE_NAME 2>/dev/null ];
 then
-    brctl addbr $BRIDGE_NAME
-    brctl addbr setfd $BRIDGE_NAME 0
-    brctl stp $BRIDGE_NAME off
+    sudo brctl addbr $BRIDGE_NAME
+    sudo brctl setfd $BRIDGE_NAME 0
+    sudo brctl stp $BRIDGE_NAME off
     if test -e /sys/class/net/$BRIDGE_NAME/bridge/multicast_snooping;
     then
-	echo 0 | tee /sys/class/net/$BRIDGE_NAME/bridge/multicast_snooping
+	echo 0 | sudo tee /sys/class/net/$BRIDGE_NAME/bridge/multicast_snooping
     fi
 fi
 
-VETH0="qvb-$BASE_ID"
-VETH1="qvo-$BASE_ID"
+VETH0="qvb$BASE_ID"
+VETH1="qvo$BASE_ID"
 
-# TODO: error handling
-
-if ! [ ip link show $VETH1 2>/dev/null ];
+if test "x$1" == "xplug";
 then
-    ip link delete $VETH0
-    ip link add $VETH0 type veth peer name $VETH1
-    ip link set $VETH0 up
-    ip link set $VETH0 promisc on
-    ip link set $VETH1 up
-    ip link set $VETH1 promisc on
-    ip link set $BRIDGE_NAME up
-    brctl addif $BRIDGE_NAME $VETH0
+    # TODO: error handling
+
+    if ! [ ip link show $VETH1 2>/dev/null ];
+    then
+	sudo ip link delete $VETH0
+	sudo ip link add $VETH0 type veth peer name $VETH1
+	sudo ip link set $VETH0 up
+	sudo ip link set $VETH0 promisc on
+	sudo ip link set $VETH1 up
+	sudo ip link set $VETH1 promisc on
+	sudo ip link set $BRIDGE_NAME up
+	sudo brctl addif $BRIDGE_NAME $VETH0
+    fi
+
+    sudo ovs-vsctl -- --if-exists del-port br-int $VETH1 -- add-port br-int $VETH1 --\
+    set Interface $VETH1 external-ids:iface-ids=$VIF_ID \
+	external-ids:iface-status=active \
+	external-ids:attached-mac=$VIF_MAC_ADDRESS \
+	external-ids:vm-uuid=$VIF_INSTANCE_ID
 fi
 
-ovs-vsctl -- --if-exists del-port $VETH1 -- add-port br-int $VETH1 --\
-    set Interface $VETH1 external-ids:iface-ids=$VIF_ID \
-    external-ids:iface-status active \
-    external-ids:attached-mac=$VIF_MAC_ADDRESS \
-    external-ids:vm-uuid=$VIF_INSTANCE_ID
-
+if test "x$1" == "xunplug";
+then
+    sudo brctl delif $BRIDGE_NAME $VETH0
+    sudo ip link set $BRIDGE_NAME down
+    sudo brctl delbr $BRIDGE_NAME
+    sudo ovs-vsctl -- --if-exists del-port br-int $VETH1
+fi
